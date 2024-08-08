b615115b0c2f9ec12b3e4467d321147668909380
Pierre-Jean Coudert <pierrejean.coudert@gmail.com>
2024-08-08 14:40:13 +0200

    add Apache licence

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from fasthtml.common import *
 
-app,rt = fast_app()
+app, rt = fast_app()
+
 
 # kamal health endpoint
-@rt('/healthz')
-def get(): return 'OK'
+@rt("/healthz")
+def get():
+    return "OK"
 
-@rt('/change')
-def get(): return P('Nice to be here!')
 
-@rt('/')
-def get(): return Div(P('Hello World!'), hx_get="/change")
+# dynamic endpoint
+@rt("/change")
+def get():
+    return P("Nice to be here!")
+
+
+# home
+@rt("/")
+def get():
+    return Div(
+        H1("I'm Deployed :-)"), 
+        Div(P("Hello from FastHtml! (click me)"), hx_get="/change")
+    )
+
 
 serve(port=8000)
